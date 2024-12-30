c9e49ad933e28513fb3d26152119c5bd513da6d7
Jeet Soni <jeet.soni0822@gmail.com>
2024-12-30 16:15:11 -0500

    finds greatest divisor

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,14 @@
+public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second){
+        if (first < 10 || second < 10){
+            return -1;
+        }
+        while (second != 0){
+            int temp = second;
+            second = first % second;
+            first = temp;
+        }
+        return first;
+
+    }
+}
