a8bf535f5535ff6e5b9a18a01c3a584512a2df50
Jeet Soni <jeet.soni0822@gmail.com>
2024-12-30 16:21:32 -0500

    added comments

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,14 +1,18 @@
 public class GreatestCommonDivisor {
     public static int getGreatestCommonDivisor(int first, int second){
+        // Checks for single digit number
         if (first < 10 || second < 10){
             return -1;
         }
+
+        // formula to find the greatest common divisor
         while (second != 0){
             int temp = second;
             second = first % second;
             first = temp;
         }
-        return first;
+
+        return first; // returns the divisor
 
     }
 }
--- src/LargestPrime.java
+++ src/LargestPrime.java
@@ -1,20 +1,22 @@
 public class LargestPrime {
         public static int getLargestPrime(int number) {
+
+            // Checks if number is less than 2
             if (number < 2) {
                 return -1;
             }
 
             int largestPrime = 0;
 
+            // finds the factor of number and checks for the largest prime
             for (int i = 2; i <= number; i++) {
-                boolean isPrime = PrimeNumber.isPrimeNumber(i);
+                boolean isPrime = PrimeNumber.isPrimeNumber(i);  // prime number checker method
 
+                // checks for largest prime
                 if (number % i == 0 && isPrime) {
                     largestPrime = i;
                 }
             }
             return largestPrime;
         }
-
-
 }
